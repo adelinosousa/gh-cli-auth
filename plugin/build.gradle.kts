6a2bd44ebb6de8b1bd85f257eeed287de2eb8b3b
uways.e <u-ways-e@users.noreply.github.com>
2025-11-06 11:13:14 +0000

    fix: get rid of all the project warnings, align kotlin version to embedded version, and correctly set functional tests as test sources rather than main

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
-
 plugins {
     `kotlin-dsl`
     alias(libs.plugins.kotlin.jvm)
@@ -7,22 +5,48 @@ plugins {
 }
 
 group = "io.github.adelinosousa"
+
 version = System
     .getenv("GRADLE_PUBLISH_VERSION")
     ?: requireNotNull(project.property("gradle.publish.version"))
 
-kotlin {
-    explicitApi = ExplicitApiMode.Strict
+dependencies {
+    testImplementation(libs.mockk)
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(libs.mockk)
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+@Suppress("UnstableApiUsage")
+testing.suites.register<JvmTestSuite>("functionalTest").configure {
+    dependencies { implementation(gradleTestKit()) }
+    targets { all { testTask.configure { useJUnitPlatform() } } }
+    configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
+    configurations["functionalTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
+    tasks.named<Task>("check") { dependsOn(this@configure) }
+}
+
+/**
+ * Enable strict explicit API mode for this project as this is a public plugin.
+ */
+kotlin.explicitApi = org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Strict
+
+tasks.named<Test>("test") {
+    /**
+     * Byte-buddy-agent is a dependency of mockk, and it is not a serviceability tool.
+     * So, we need to enable dynamic agent loading to hide the warning and make it work in future JDK versions.
+     */
+    jvmArgs("-XX:+EnableDynamicAgentLoading")
+    /**
+     * > OpenJDK 64-Bit Server VM warning:
+     * > Sharing is only supported for bootloader classes because bootstrap classpath has been appended
+     * For further details, see: https://github.com/gradle/gradle/issues/19989 is resolved.
+     */
+    jvmArgs("-Xshare:off")
+    useJUnitPlatform()
 }
 
 gradlePlugin {
@@ -33,40 +57,15 @@ gradlePlugin {
             id = "io.github.adelinosousa.gradle.plugins.project.gh-cli-auth"
             implementationClass = "io.github.adelinosousa.gradle.plugins.GhCliAuthProjectPlugin"
             displayName = "Gradle GitHub CLI Auth Project Plugin"
-            description =
-                "Automatically configures access to GitHub Maven Packages for project using gh CLI. CI/CD friendly."
+            description = "Automatically configures access to GitHub Maven Packages for project using gh CLI. CI/CD friendly."
             tags.set(listOf("github", "packages", "maven", "repository"))
         }
         register("ghCliAuthSettings") {
             id = "io.github.adelinosousa.gradle.plugins.settings.gh-cli-auth"
             implementationClass = "io.github.adelinosousa.gradle.plugins.GhCliAuthSettingsPlugin"
             displayName = "Gradle GitHub CLI Auth Settings Plugin"
-            description =
-                "Automatically configures access to GitHub Maven Plugins for settings using gh CLI. CI/CD friendly."
+            description = "Automatically configures access to GitHub Maven Plugins for settings using gh CLI. CI/CD friendly."
             tags.set(listOf("github", "packages", "maven", "repository"))
         }
     }
-}
-
-// Add a source set for the functional test suite
-val functionalTestSourceSet = sourceSets.create("functionalTest")
-
-configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
-configurations["functionalTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
-
-// Add a task to run the functional tests
-val functionalTest by tasks.registering(Test::class) {
-    testClassesDirs = functionalTestSourceSet.output.classesDirs
-    classpath = functionalTestSourceSet.runtimeClasspath
-    useJUnitPlatform()
-}
-
-gradlePlugin.testSourceSets.add(functionalTestSourceSet)
-
-tasks.named<Task>("check") {
-    dependsOn(functionalTest)
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
 }
